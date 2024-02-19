e2556119ff2ed846c6209392b2c2091442c1ddb8
Elly Olivebring <ellyolivebring@MacBook-Air-som-tillhor-Elly.local>
2024-02-19 14:19:01 +0100

    Jag la till en fetch i new User för att hämta karaktärer

--- src/views/newUser.vue
+++ src/views/newUser.vue
@@ -1,5 +1,32 @@
-<script></script>
+<script>
+export default{
+    data(){
+        return{
+            characterNames: []
+        };
+    },
+
+mounted(){
+        fetch("https://swapi.dev/api/people/1")
+        .then(response => response.json())
+        .then(data => {
+            this.characterNames = data;
+        })
+
+}
+
+}
+
+</script>
 <template>
-    <p >Username: {{ }}</p>
+    <div>
+        <h1>Show Characters</h1>
+
+        <ul>
+            <li v-for="name in characterNames" :key="name">{{ name }}</li>
+        </ul>
+
+    </div>
+
 
 </template>
