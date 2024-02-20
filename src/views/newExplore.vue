f36d8085c96e4f795101226b9f408d9b13a262bc
Elly Olivebring <ellyolivebring@MacBook-Air-som-tillhor-Elly.local>
2024-02-20 20:11:27 +0100

    fixade en ändrad fil

--- src/views/newExplore.vue
+++ src/views/newExplore.vue
@@ -40,7 +40,11 @@ methods:{
     <b-card-text>
         <p>{{ character.name }}</p>
     </b-card-text>
-    <b-button href="#" variant="primary">Go somewhere</b-button>
+    <div>
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+    <b-modal id="modal-1" title="BootstrapVue">
+    <p class="my-4">Hello from modal!</p>
+  </b-modal></div>
   </b-card></b-col></b-row>
 </div>
 
